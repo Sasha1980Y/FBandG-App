97acd9f25b708198103c2a2da776241673ce5656
AlexanderYakovenko <AlexanderYakovenko@Hellens-Mac-mini.local>
2017-07-25 16:11:30 +0300

    FB and Tabbar

---
+++ FLoginB/TabBar.swift
@@ -0,0 +1,35 @@
+//
+//  TabBar.swift
+//  FLoginB
+//
+//  Created by AlexanderYakovenko on 7/25/17.
+//  Copyright © 2017 AlexanderYakovenko. All rights reserved.
+//
+
+import UIKit
+
+class TabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FLoginB/ViewController.swift
+++ FLoginB/ViewController.swift
@@ -7,19 +7,98 @@
 //
 
 import UIKit
+import FacebookLogin
+import FacebookShare
+import FacebookCore
 
-class ViewController: UIViewController {
 
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    // FB 1
+    let loginButton: FBSDKLoginButton = {
+        let button = FBSDKLoginButton()
+        button.readPermissions = ["email"]
+        return button
+    }()
+    // FB 1 end
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+        // FB 2
+        view.addSubview(loginButton)
+        loginButton.center = view.center
+        loginButton.delegate = self
+        
+        
+        if let token = FBSDKAccessToken.current() {
+            //fetchProfile()
+            
+            //goToTabBarController()
+        }
+        // FB 2 end
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    
+    // FB 3
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print("completed login")
+        
+        goToTabBarController()
+        
     }
-
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+    }
+    func loginButtonWillLogin(_ loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
+    
+    func fetchProfile() {
+        print("fetch Profile")
+        
+        let parameters = ["fields": "email, first_name, last_name, picture.type(large)"]
+        
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).start {(connection, result, error) -> Void in
+            if error != nil {
+                print(error!)
+            }
+            
+            if let email = (result as AnyObject)["email"]! as? String {
+                print(email)
+            }
+           print(result!)
+           
+        }
+        
+    }
+    // FB 3 end
+    
+    
+    
+    
+    func goToTabBarController(){
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "TabBar") as! TabBar
+        //navigationController?.pushViewController(vc, animated: true)
+        present(vc, animated: true, completion: nil)
+    }
+    
+    
+    
 
 }
 
